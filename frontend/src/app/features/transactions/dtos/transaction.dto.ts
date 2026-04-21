151c645ddec79222a7927d80f7a13650af205af1
Angel Montiel Pereña <A513640@AT4WIRELESS>
2026-04-21 13:33:38 +0200

    feat: Implemented types, interfaces and mappers

--- frontend/src/app/features/transactions/dtos/transaction.dto.ts
+++ frontend/src/app/features/transactions/dtos/transaction.dto.ts
@@ -1,9 +1,11 @@
 import { User } from '../../auth/interfaces/user.interface';
 import { PaymentStatus } from '../enums/payment-status.enum';
 import { TransactionStatus } from '../enums/transaction-status.enum';
+import { TransactionItem } from '../interface/transaction-item.interface';
 
 export interface TransactionDto {
   user: User;
+  transactions_items: TransactionItem[];
   status: TransactionStatus;
   subtotal: number;
   discount_applied: number;
