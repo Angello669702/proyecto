b1740f44fed5c6c44d22f7c27918b8bd5d780b24
Ángel <montielperenaangel@gmail.com>
2026-05-10 18:07:14 +0200

    chore

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -4,8 +4,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
-        'http://51.91.110.54',
-        'http://localhost:4200',
+        '*',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'Accept', 'Origin'],
