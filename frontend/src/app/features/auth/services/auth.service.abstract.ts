626da91576fe6f0e286283e5c671a9b0331293a0
Ángel <montielperenaangel@gmail.com>
2026-05-10 15:41:51 +0200

    chore

--- frontend/src/app/features/auth/services/auth.service.abstract.ts
+++ frontend/src/app/features/auth/services/auth.service.abstract.ts
@@ -3,7 +3,7 @@ import { User } from '../interfaces/user.interface';
 import { AuthRequest, AuthResponse } from '../interfaces/auth.interface';
 
 export abstract class AuthServiceAbstract {
-  protected readonly API_ENDPOINT = 'http://localhost:8000/api/auth';
+  protected readonly API_ENDPOINT = 'http://51.91.110.54:8000/api/api/auth';
 
   readonly defaultUser: User = {
     id: '0',
